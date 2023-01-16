ccfd70898e4109e1b4670760b76cf12c794e6c58
Nitesh <niteshchandrakar2@gmail.com>
2023-01-16 17:54:10 +0530

    prime

---
+++ prime.js
@@ -0,0 +1,19 @@
+let n=7
+
+let count=0
+
+for(let i=2;i<=n;i--){
+      
+     if(n%i==0){
+           ccount++
+                   }
+
+    }
+if(count==1){
+             console.log("prime number") 
+                       }
+
+
+else{
+          console.log("not a prime number")
+                    }
